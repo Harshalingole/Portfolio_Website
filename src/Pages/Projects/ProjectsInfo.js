0350619e90049b2c9a75a0bea797741969037f53
Harshalingole <harshal.ingole21@gmail.com>
2022-08-18 19:27:50 +0530

    Adeded Two More Projects: Dashboard & Urbanfit

--- src/Pages/Projects/ProjectsInfo.js
+++ src/Pages/Projects/ProjectsInfo.js
@@ -2,35 +2,59 @@ import Todo from "../../img/Project/Todo.png";
 import Kryptic from "../../img/Project/Kryptic.png";
 import Architect from "../../img/Project/Architect.png";
 import Bank from "../../img/Project/Bank.png";
+import UrbanFit from "../../img/Project/Urbanfit-1.png";
+import Dashboard from "../../img/Project/Dashboard-2.png";
 const ProjectsInfo = [
   {
-    title: ["Todo", "Kryptic", "Architects", "Bank"],
+    title: [
+      "Modern-Dashboard",
+      "Urbanfit Landing Page",
+      "Todo",
+      "Kryptic",
+      "Architects",
+      "Bank",
+    ],
     info: [
+      "Converted Figma Prototype Design into real webapp using react js.Divided whole project code structure into individual components and build scalable,reusable components.Must Have Look Through Code Structure To Get Inside.",
+      "Made an Landing Page for Urbanfit franchise business.Made Design from Scratch which fit to their brand",
       "React Todo App with functionality of  1) Add New Task, 2)Mark As Complete , 3)Delete Task, 4)Total Task Count, 5)Remaining Task Count, 6)Completed Task Count.Responsive and Good Design",
       "Kryptic is  React Project in which You can search for your favorite crypto currencies and get latest : Price ,24hr price change, market Cap of cryptocurrincis .",
       "Truly Responsive on every Device! A Static Web Page build using power of Grid,Flexbox and  without using single Media queries with just 200 line of CSS.",
       "Basic Bank User Interface to perform Transfer of money and show list of transaciton.Build Using Object Oriented Programing .",
     ],
     github: [
+      "https://github.com/Harshalingole/Dashboard",
+      "https://github.com/Harshalingole/UrbanfitLandingPage",
       "https://github.com/Harshalingole/To-Do-App",
       "https://github.com/Harshalingole/Kryptic",
       "https://github.com/Harshalingole/Projects/tree/main/Beginner/RWD-Temp/Architect-Temp",
       "https://codepen.io/Harshalingole/pen/vYpqjNK",
     ],
     liveDemo: [
-      "https://shimmering-beignet-7a6458.netlify.app/",
-      "https://velvety-buttercream-5a5051.netlify.app/",
+      "https://modern-dasboard.netlify.app/",
+      "https://urbanfitlandigpage.netlify.app/",
+      "https://todo-app-hgi.netlify.app/",
+      "https://kryptic-mart.netlify.app/",
       "https://codepen.io/Harshalingole/full/WNMoLjO",
       "https://codepen.io/Harshalingole/full/vYpqjNK",
     ],
     stack: [
+      ["React", "UseState", "Router", "HTML", "CSS", "JS", "Tailwind"],
+      ["React", "UseState", "HTML", "CSS", "JS", "Tailwind"],
       ["React", "UseState", "HTML", "CSS", "JS"],
       ["React", "fetch", "API", "HTML", "CSS", "JS"],
       ["HTML", "CSS", "GRID", "FlexBox"],
       ["HTML", "CSS", "GRID", "JS", "Prototype Inheritence", "Classes"],
     ],
-    date: ["JUN 2021", "MAY 2021", "JAN 2021", "March 2021"],
-    img: [Todo, Kryptic, Architect, Bank],
+    date: [
+      "AUG 2022",
+      "AUG 2022",
+      "JUN 2022",
+      "MAY 2022",
+      "JAN 2022",
+      "March 2022",
+    ],
+    img: [Dashboard, UrbanFit, Todo, Kryptic, Architect, Bank],
   },
 ];
 
